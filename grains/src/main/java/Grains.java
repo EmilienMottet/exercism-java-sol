07041a1a9a5ac5dd23e830c75c4d42f7310ba3cd
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-26 15:59:52 +0100

    clean grain

--- grains/src/main/java/Grains.java
+++ grains/src/main/java/Grains.java
@@ -7,11 +7,11 @@ class Grains {
     if (square < 1 || square > 64) {
       throw new IllegalArgumentException("square must be between 1 and 64");
     }
-    return new BigInteger("1").shiftLeft(square - 1);
+    return BigInteger.ONE.shiftLeft(square - 1);
   }
 
   BigInteger grainsOnBoard() {
-    return IntStream.rangeClosed(1, 64).mapToObj((i) -> grainsOnSquare((int)i)).reduce(BigInteger::add).get();
+    return IntStream.rangeClosed(1, 64).mapToObj(i -> grainsOnSquare(i)).reduce(BigInteger::add).get();
   }
 
 }
